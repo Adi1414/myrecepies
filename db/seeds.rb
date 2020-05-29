990d96a0503ee7e5466a33fd0598588ca41d4280
Adi1414 <aditiagrawal1414@gmail.com.com>
2020-05-29 20:55:58 +0530

    sdded seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Chef.create(chefname: "Aditi", email: "aditi@gmail.com", password: "jewel", password_confirmation: "jewel", admin: true)
+@chef = Chef.create!(chefname: "mashrur",email: "mashrur@example.com", password: "jewel", password_confirmation: "jewel")
+@recipe = Recipe.create(name: "vegetable saute",description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+
+
+@chef = Chef.create!(chefname: "chuchu",email: "chuchu@example.com", password: "jewel", password_confirmation: "jewel")
+@admin = Chef.create!(chefname: "tomato1",email: "tomato1@example.com", password: "jewel", password_confirmation: "jewel", admin: true)
+
+@recipe = Recipe.create(name: "Paneer tikka",description: "great vegetable sautee, add vegetable and oil", chef: @chef)
