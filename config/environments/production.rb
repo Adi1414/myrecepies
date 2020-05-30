7c4ec9ae0a3992d73e4dfda4f2de0120faadf889
Adi1414 <aditiagrawal1414@gmail.com.com>
2020-05-30 10:26:50 +0530

    action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.web_socket_server_url = "wss://aditi-myrecipe.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://aditi-myrecipe.herokuapp.com', 'http://aditi-myrecipe.herokuapp.com']
+  # config.web_socket_server_url = "wss://aditi-myrecipe.herokuapp.com/cable"
+  # config.action_cable.allowed_request_origins = ['https://aditi-myrecipe.herokuapp.com', 'http://aditi-myrecipe.herokuapp.com']
 
 end
